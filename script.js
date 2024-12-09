b1f1c4eddcae1d6a423035b79fecb5b13382c91d
JuliaSPalmeira <serennamares@gmail.com>
2024-12-09 17:51:06 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const cards = document.querySelectorAll(".card");
 
-let
+let matchedCard = 0;
 let cardOne, cardTwo;
 let disableDeck = false; //não poder clicar em varios cards de uma vez
 
@@ -22,27 +22,54 @@ function flipCard(e) {
 
 function matchCards(img1, img2) {
     if (img1 === img2) {//if two cards img matched
-        cardOne.removeEventListener("click",flipCard);
-        cardTwo.removeEventListener("click",flipCard);
-        cardOne =  cardTwo = ""; //setting both card value to blank pra pode resetar e poder escolher dois novos card
+        matchedCard++;//increment matched value by 
+        //if matched value is 8 that means user has macthed all the cards (8*2 = 16 cards)
+        if (matchedCard == 8) {
+            setTimeout(() => {
+                return shuffleCard();
+            }, 1000); //calling shufflCard function after 1 s
+
+        }
+        cardOne.removeEventListener("click", flipCard);
+        cardTwo.removeEventListener("click", flipCard);
+        cardOne = cardTwo = ""; //setting both card value to blank pra pode resetar e poder escolher dois novos card
         return disableDeck = false;  // pro card poder ficar caso match
     }
     // caso n match ele shake
     setTimeout(() => {
         //adding shake class to both card after 400ms
-        cardOne.classList.add("shake"); 
+        cardOne.classList.add("shake");
         cardTwo.classList.add("shake");
     }, 400);
 
-     // caso n match ele shake
-     setTimeout(() => {
+    // caso n match ele shake
+    setTimeout(() => {
         //removing both shake e flip classes from the both card after 1.2s
-        cardOne.classList.remove("shake", "flip"); 
+        cardOne.classList.remove("shake", "flip");
         cardTwo.classList.remove("shake", "flip");
-        cardOne =  cardTwo = ""; //setting both card value to blank pra pode resetar e poder escolher dois novos card
+        cardOne = cardTwo = ""; //setting both card value to blank pra pode resetar e poder escolher dois novos card
         disableDeck = false;
     }, 1200);
 }
+
+function shuffleCard() {
+    matchedCard = 0;
+    cardOne = cardTwo = "";
+    disableDeck = false;
+    //creating array of 16 itens and each item is repeated twice
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8,]
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1); ///sorting array item randomly
+
+    //removing flip class from all cards and passing ramdom image to each card
+    cards.forEach((card, index) => {
+        card.classList.remove("flip");
+        let imgTag = card.querySelector("img");
+        imgTag.src = `Memory Card Game Images/img-${arr[index]}.png`;
+        card.addEventListener("click", flipCard);
+    });
+}
+
+shuffleCard();
 
 cards.forEach(card => { //adding click event to all cards
     card.addEventListener("click", flipCard);
